234f1087bc173eae817a65c3320cc30db72c74f2
FantomStudy <arzamastsevdaniil@gmail.com>
2025-05-28 15:52:09 +0500

    style: update CSS styles for container and define root variables

--- src/app/main.tsx
+++ src/app/main.tsx
@@ -1,8 +1,8 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./styles/index.css";
 import { RouterProvider } from "@tanstack/react-router";
 import { router } from "./init/router";
+import "./styles/index.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
