e344ecacd28188127e43a587bbca3151a25340e9
visstt <egorskomorohov020606@gmail.com>
2025-05-29 11:43:32 +0500

    update: Implement multiple code changes to enhance functionality and improve performance

--- src/pages/index-page/IndexPage.tsx
+++ src/pages/index-page/IndexPage.tsx
@@ -2,36 +2,39 @@ import { Button } from "@shared/ui/button/Button";
 import styles from "./IndexPage.module.css";
 const IndexPage = () => {
   return (
-    <div className="container">
-      <div className={styles.indexPage}>
-        <div className={styles.label}>
-          <h1>Система тестирования</h1>
-          <h2>Вход</h2>
-        </div>
-        <form className={styles.form}>
-          <p>Номинация</p>
-          <select className={styles.select}>
-            <option value="" disabled selected>
-              Выберите номинацию
-            </option>
-            <option value="1">Номинация 1</option>
-            <option value="2">Номинация 2</option>
-            <option value="3">Номинация 3</option>
-          </select>
-          <p>Номер</p>
-          <input type="text" />
-
-          <div className={styles.time}>
-            <h2>Время теста</h2>
-            <h2 className={styles.blue}>45:00</h2>
+    <>
+      <div className="container">
+        <div className={styles.indexPage}>
+          <div className={styles.label}>
+            <h1>Система тестирования</h1>
+            <p>Вход</p>
           </div>
+          <form className={styles.form}>
+            <p>Номинация</p>
+            <select className={styles.select}>
+              <option value="" disabled selected>
+                Выберите номинацию
+              </option>
+              <option value="1">Номинация 1</option>
+              <option value="2">Номинация 2</option>
+              <option value="3">Номинация 3</option>
+            </select>
+            <p>Номер</p>
+            <input type="text" />
 
-          <Button size="l" color="primary">
-            Войти
-          </Button>
-        </form>
+            <div className={styles.time}>
+              <p>Время теста</p>
+              <p className={styles.blue}>45:00</p>
+            </div>
+
+            <Button size="l" color="primary">
+              Войти
+            </Button>
+          </form>
+        </div>
       </div>
-    </div>
+      <img src="/wave-mask.png" alt="wave" className={styles.wave} />
+    </>
   );
 };
 
