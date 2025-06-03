154dafcf39fc04d065343635cb1fddb74c779f38
visstt <egorskomorohov020606@gmail.com>
2025-06-03 12:58:15 +0500

    fix: fix adaptive

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -4,8 +4,14 @@ import { RouterProvider } from "@tanstack/react-router";
 import { queryClient } from "./init/queryClient";
 import { router } from "./init/router";
 import "./styles/App.css";
+import { useEffect } from "react";
 
 function App() {
+
+  useEffect(()=>{
+    console.log('(.)(.)');
+  }) 
+
   return (
     <QueryClientProvider client={queryClient}>
       <RouterProvider router={router} />;
