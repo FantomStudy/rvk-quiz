5c0151f991e9aeafa31a797989047380d1c72cec
FantomStudy <arzamastsevdaniil@gmail.com>
2025-08-27 09:42:05 +0300

    feat: add sign under table car

--- src/modules/admin/statistic/practice/components/driver/TruckDriver/TruckDriver.tsx
+++ src/modules/admin/statistic/practice/components/driver/TruckDriver/TruckDriver.tsx
@@ -19,69 +19,77 @@ export const TruckDriver = ({ sortBy }: SortProps) => {
   const sortedData = sortWithEmptyLast(data, sortBy);
 
   return (
-    <Table className={styles.table}>
-      <DriverHead />
-      <tbody>
-        {sortedData.map(({ user, theory, practice, result, ...row }) => (
-          <tr key={row.id}>
-            <td>{row.branch.address}</td>
-            <EditableCell
-              save={(value) =>
-                line.mutate({
-                  lineNumber: Number(value),
-                  userId: user.id,
-                  practicNominationId: row.practicNominationId,
-                })
-              }
-              initialValue={row.lineNumber?.toString() || ""}
-            >
-              {row.lineNumber}
-            </EditableCell>
-            <td>{user.fullName}</td>
+    <>
+      <Table className={styles.table}>
+        <DriverHead />
+        <tbody>
+          {sortedData.map(({ user, theory, practice, result, ...row }) => (
+            <tr key={row.id}>
+              <td>{row.branch.address}</td>
+              <EditableCell
+                save={(value) =>
+                  line.mutate({
+                    lineNumber: Number(value),
+                    userId: user.id,
+                    practicNominationId: row.practicNominationId,
+                  })
+                }
+                initialValue={row.lineNumber?.toString() || ""}
+              >
+                {row.lineNumber}
+              </EditableCell>
+              <td>{user.fullName}</td>
 
-            <td>{theory.correct}</td>
-            <td>{theory.time}</td>
-            <td>{theory.place}</td>
-            <td>{theory.points}</td>
+              <td>{theory.correct}</td>
+              <td>{theory.time}</td>
+              <td>{theory.place}</td>
+              <td>{theory.points}</td>
 
-            <EditableCell
-              save={(value) =>
-                mutate({
-                  userId: user.id,
-                  practiceTime: practice.time,
-                  practicePenalty: Number(value),
-                })
-              }
-              initialValue={practice.penalty.toString()}
-            >
-              {practice.penalty}
-            </EditableCell>
+              <EditableCell
+                save={(value) =>
+                  mutate({
+                    userId: user.id,
+                    practiceTime: practice.time,
+                    practicePenalty: Number(value),
+                  })
+                }
+                initialValue={practice.penalty.toString()}
+              >
+                {practice.penalty}
+              </EditableCell>
 
-            <EditableCell
-              isTime
-              save={(value) =>
-                mutate({
-                  userId: user.id,
-                  practicePenalty: practice.penalty,
-                  practiceTime: value,
-                })
-              }
-              initialValue={practice.time}
-            >
-              {practice.time}
-            </EditableCell>
-            
-            <td>{practice.sum}</td>
-            <td>{practice.place}</td>
-            <td>{practice.points}</td>
+              <EditableCell
+                isTime
+                save={(value) =>
+                  mutate({
+                    userId: user.id,
+                    practicePenalty: practice.penalty,
+                    practiceTime: value,
+                  })
+                }
+                initialValue={practice.time}
+              >
+                {practice.time}
+              </EditableCell>
 
-            <td>{result.theoryPoints}</td>
-            <td>{result.practicePoints}</td>
-            <td>{result.points}</td>
-            <td>{row.place}</td>
-          </tr>
-        ))}
-      </tbody>
-    </Table>
+              <td>{practice.sum}</td>
+              <td>{practice.place}</td>
+              <td>{practice.points}</td>
+
+              <td>{result.theoryPoints}</td>
+              <td>{result.practicePoints}</td>
+              <td>{result.points}</td>
+              <td>{row.place}</td>
+            </tr>
+          ))}
+        </tbody>
+      </Table>
+      <p style={{ fontSize: 8, margin: "15px 0" }}>
+        При условии равного количества итоговых баллов конкурсной комиссией
+        принято решение комплексно оценить участников по всем результатам
+        соревнований с приоритетом на практическую часть (штрафные баллы, время,
+        затраченное на прохождение дистанции)
+      </p>
+    </>
   );
 };
